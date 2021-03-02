855dc41341dae1b448ac5f3d93c397cdb121d344
jmoor147 <59812781+jmoor147@users.noreply.github.com>
2021-03-01 21:08:12 -0500

    BasicCombat
    Added script for damaging the zombie. The zombie will receive damage until it's health reaches 0 and then fall backwards.

--- Horror Game/Assets/Scripts/FirePistol.cs
+++ Horror Game/Assets/Scripts/FirePistol.cs
@@ -8,8 +8,9 @@ public class FirePistol : MonoBehaviour
     public GameObject MuzzleFlash;
     public AudioSource GunFire;
     public bool IsFiring = false;
-
-    // Update is called once per frame
+    public float TargetDistance;
+    public int DamageAmount = 5;
+    
     void Update()
     {
         if (Input.GetButtonDown("Fire1")) {
@@ -19,7 +20,12 @@ public class FirePistol : MonoBehaviour
         }
     }
     IEnumerator FiringPistol() {
+        RaycastHit Shot;
         IsFiring = true;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot)){
+            TargetDistance = Shot.distance;
+            Shot.transform.SendMessage("DamageZombie", DamageAmount, SendMessageOptions.DontRequireReceiver);
+        }
         TheGun.GetComponent<Animation>().Play("PistolShot");
         MuzzleFlash.SetActive(true);
         MuzzleFlash.GetComponent<Animation>().Play("MuzzleAnim");
